a89003a887aadb409fa3725a1eb1503c46bcd600
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-31 10:31:45 +0200

    ok4

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$age = 18;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>Partie 2 EXE 4 PHP</title>
+</head>
+<body>
+  <?php
+  if($age >= 18){?>
+    <p>Tu es majeur</p>
+    <?php
+  }
+  else{?>
+    <p>Tu n\'es pas majeur</p>
+    <?php}?>
+  </body>
+  </html>
